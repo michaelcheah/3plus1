34327b169eb7b6573e690825090826612afbf8b6
Vivian Wong <vivian5096@gmail.com>
2017-11-25 22:38:06 +0000

    hello.py : write to csv

--- hello.py
+++ hello.py
@@ -1,34 +1,32 @@
 from flask import Flask,render_template,request,Response,json
 import sys
+import numpy as np
+import pandas as pd
 app = Flask(__name__)
 
-tempHum_list = []
-
+data_list = []
 
 
 def parse_temp(input_binary_string):
     #convert to string
     input = input_binary_string.decode('ascii')
-    temp_string = input[input.find("TEMP: ")+len("TEMP: "):input.find("C,")]
-
-    hum_string = input[input.find("HUM: ")+len("HUM: "):input.find("%")]
-
-    
-    return float(temp_string),float(hum_string)
+    #temp_string = input[input.find("TEMP: ")+len("TEMP: "):input.find("C,")]
+    #hum_string = input[input.find("HUM: ")+len("HUM: "):input.find("%")]
+    mylist = input.split(',')
+    mylist=[float(x) for x in mylist]
+    return mylist
 
 @app.route("/")
 def hello():
     #return page1
+    wfile_csv()
     return render_template("index.html")
 
 @app.route('/test' , methods=['GET'])
 def hello2():
     
     print(request.data, file=sys.stdout)
-    temp,humidity = parse_temp(request.data)
-
-    tempHum_list.append((temp,humidity))
-
+    data_list.append(parse_temp(request.data))
     
     content = b"GET: Hello, Mbed!"
     resp = Response(content)
@@ -39,10 +37,15 @@ def hello2():
 @app.route('/result')
 def output_result():
     content = "<html><body><table>"
-    for i in tempHum_list:
+    for i in data_list:
         content += ("<tr><td>"+str(i[0])+"</td><td>"+str(i[1])+"</td></tr>")
     content+="</table></body></html>"
     return content
+
+def wfile_csv():
+    data=pd.DataFrame(data_list,columns=['Temp','Humid','Co2lvl','TVOC','Visible','Infrared','Acc_x','Acc_y','Acc_z'])
+    data.to_csv('static/mydata.csv')
+    return data
 
 if __name__ == '__main__':
     #app = create_app()
