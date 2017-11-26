48e48038988d82e1c2ac5d05d96e09d2a28d7699
Vivian Wong <vivian5096@gmail.com>
2017-11-26 02:25:04 +0000

    javascript update

--- static/plot.js
+++ static/plot.js
@@ -44,33 +44,47 @@ function twoDec(num) {
     
     
 
-
 function generateData(result){
     var array1 = result.data;
     var arrayLength = array1.length;
     var temp = [];
-    for (var i = 0; i < arrayLength; i++){
-        temp.push(array1[i][0]);
+    for (var i = 1; i < arrayLength; i++){
+        output1.push(array1[i][1]);
+	output2.push(array1[i][2]);
+	output3.push(array1[i][5]);
+	output4.push(array1[i][6]);
+	output5.push(array1[i][7]);
+	output6.push(array1[i][8]);
+	output7.push(array1[i][9]);
     }
     //console.log(output1);
-    return temp;
+    //return temp;
 }
-
-var output1_plot = {};
-
-var output1 = [];    
 
 
 //console.log(output1);
 
-
-
+    var output1 = [];
+    var output1_plot = {};   
+    var output2 = [];  
+    var output2_plot = {};
+    var output3 = [];  
+    var output3_plot = {};
+    var output4 = [];  
+    var output4_plot = {};
+    var output5 = [];  
+    var output5_plot = {};
+    var output6 = [];  
+    var output6_plot = {};
+    var output7 = [];  
+    var output7_plot = {};
 
 window.onload = function(){
     
   ctx = document.getElementById('myChart').getContext('2d');
     
-    var myRequest = new Request('/static/out.csv');
+    var myRequest = new Request('/static/michael.csv');
+    
 
     fetch(myRequest)
       .then(function(response) { return response.text(); })
@@ -78,13 +92,51 @@ window.onload = function(){
         Papa.parse(csv, {
             complete: function(results) {
                 console.log(results);
-                output1 = generateData(results);
+                generateData(results);
                 output1_plot = {
                   data: output1,
-                  label: "BTC Price",
+                  label: "Temperature",
                   borderColor: "#f0ad4e",
                   fill: false,
                 }
+		output2_plot = {
+                  data: output2,
+                  label: "Humidity",
+                  borderColor: "#0052C2",
+                  fill: false,
+                }
+		output3_plot = {
+                  data: output3,
+                  label: "Visible",
+                  borderColor: "#003b8f",
+                  fill: false,
+                }
+		output4_plot = {
+                  data: output4,
+                  label: "Infra-red",
+                  borderColor: "#4794ff",
+                  fill: false,
+                }
+		output5_plot = {
+                  data: output5,
+                  label: "Acceleration_x",
+                  borderColor: "#9500cb",
+                  fill: false,
+                }
+		output6_plot = {
+                  data: output6,
+                  label: "Acceleration_y",
+                  borderColor: "#5cb85c",
+                  fill: false,
+                }
+		output7_plot = {
+                  data: output7,
+                  label: "Acceleration_z",
+                  borderColor: "#e27fef",
+                  fill: false,
+                }
+console.log(output1)
+console.log(output2)
                 
                 timeRange=output1.length
     
@@ -92,10 +144,9 @@ window.onload = function(){
                   myChart = new Chart(ctx, {
                     type: 'line',
                     data: {
-                      labels: makeTimeLabel(timeRange,10,false),
+                      labels:Array.apply(null, Array(timeRange)).map(function (_, i) {return i;}),
                       datasets: [
-                        output1_plot,
-                      ],
+                        output1_plot,output2_plot,output3_plot,output4_plot,output5_plot,output6_plot,output7_plot],
                     }
                   });
                 
